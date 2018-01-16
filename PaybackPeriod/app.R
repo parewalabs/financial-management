654cb897a31fccdaafd1bb8a79208bf1813c3a21
Punit Jajodia <punit@parewalabs.com>
2018-01-16 22:40:27 +0545

    Added first project for Payback period and discounted payback period calculation

---
+++ PaybackPeriod/app.R
@@ -0,0 +1,95 @@
+library("shiny")
+library("ggplot2")
+
+capital_investment <- 40000
+
+cash_flow <- "
+Year,CashFlow
+1,-17500
+2,20000
+3,25000
+4,25000
+5,25000"
+
+df <- read.table(text = cash_flow, sep =",", header = TRUE, stringsAsFactors = FALSE)
+print(df$Year)
+
+ui <- fluidPage(titlePanel("Payback Period Example"),
+                br(), br(),
+                sidebarLayout(
+                  sidebarPanel(sliderInput("capitalInput", "Initial Capital", min = 0, max = 100000,
+                                           value = capital_investment, pre = "$"),
+                               sliderInput("discountRate", "Discount Rate", min=0, max=30,
+                                           value=10, post = "%"),
+                               textAreaInput("cashflowInput", "Cash flows", value = cash_flow, width = NULL, height = NULL,
+                                             cols = NULL, rows = 10, placeholder = NULL, resize = NULL)
+                  ),
+                  mainPanel(
+                    plotOutput("cashflowsBarChart"),
+                    br(), br(),
+                    h4("Normal payback period"), 
+                    h2(textOutput("paybackPeriod")),
+                    br(),
+                    h4("Discounted payback period"),
+                    h2(textOutput("discountedPaybackPeriod"))
+                    
+                  )))
+server <- function(input, output) {
+  cashflow_df <- reactive({
+    read.table(text = input$cashflowInput, sep =",", header = TRUE, stringsAsFactors = FALSE) 
+  })
+  
+  paybackPeriod <- function(discountRate) {
+    
+  }
+  
+  output$paybackPeriod <- renderText({
+    capital <- input$capitalInput
+    
+    sum <- 0
+    index <- 1
+    for(i in cashflow_df()$CashFlow) {
+      sum <- sum + i
+      if(sum >= capital) {
+        break
+      }
+      index <- index + 1
+    }
+    lastYearCashFlow <- cashflow_df()$CashFlow[index]
+    
+    amountExceedingCapital <- sum - capital
+    
+    index <- index - amountExceedingCapital/lastYearCashFlow
+    
+    index <- round(index, digits=2)
+    return(index)
+  })
+  
+  output$discountedPaybackPeriod <- renderText({
+    capital <- input$capitalInput
+    discountRate <- input$discountRate
+    
+    sum <- 0
+    index <- 1
+    for(i in cashflow_df()$CashFlow) {
+      sum <- sum + i/((1+discountRate/100)^index)
+      if(sum >= capital) {
+        break
+      }
+      index <- index + 1
+    }
+    
+    lastYearCashFlow <- cashflow_df()$CashFlow[index]
+    amountExceedingCapital <- sum - capital
+    index <- index - amountExceedingCapital/lastYearCashFlow
+    index <- round(index, digits=2)
+    
+    return(index)
+  })
+  
+  output$cashflowsBarChart <- renderPlot({
+    cashflow_df()$CashFlow
+    ggplot(cashflow_df(), aes(Year, CashFlow)) + geom_bar(stat = "identity")
+  })
+}
+shinyApp(ui = ui, server = server)
